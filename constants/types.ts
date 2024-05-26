50c6a16c9e368ad16ba474e46c3ad614a2def15c
turanmustafaa <mustafaturan502@gmail.com>
2024-05-27 00:36:46 +0300

    fourth commit

--- constants/types.ts
+++ constants/types.ts
@@ -1,19 +1,15 @@
-// types.ts (veya uygun bir dosya)
 export interface Character {
     id: number;
     name: string;
-    // Diğer gerekli alanlar...
   }
   
   export interface Location {
     id: number;
     name: string;
-    // Diğer gerekli alanlar...
   }
   
   export interface Episode {
     id: number;
     name: string;
-    // Diğer gerekli alanlar...
   }
   
